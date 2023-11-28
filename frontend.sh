e5d97df37020cf6e55c42515111943ee5547a307
pappik <pappik@gmail.com>
2023-11-28 17:18:45 -0500

    route 53 json

--- frontend.sh
+++ frontend.sh
@@ -4,8 +4,8 @@ rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-cd /usr/share/nginx/html/
-cp /tmp/frontend.zip
+cp /tmp/frontend.zip /usr/share/nginx/html/
+unzip frontend.zip
 
 cp /files/roboshop.conf /usr/share/nginx/html/
 
