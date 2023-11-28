1eaf755a1c308742d57e6c61a3375ded4342ebcf
pappik <pappik@gmail.com>
2023-11-28 17:41:18 -0500

    route 53 json

--- frontend.sh
+++ frontend.sh
@@ -3,15 +3,13 @@ script_location=$(pwd)
 dnf install nginx -y
 systemctl enable nginx
 
-systemctl restart nginx
+systemctl start nginx
 
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 unzip /tmp/frontend.zip
 
-cp ${script_location}/files/roboshop.conf /etc/nginx/default.d/roboshop.conf -y
-
-systemctl enable nginx
+cp ${script_location}/files/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl restart nginx
