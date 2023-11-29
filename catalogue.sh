a8a015c865e3c41fb35a0012b3ad02c902693680
pappik <pappik@gmail.com>
2023-11-29 10:30:20 -0500

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,13 @@
 source common.sh
 
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+dnf module disable nodejs -y &>>{LOG}
+statuscheck
 
-dnf install nodejs -y
+dnf module enable nodejs:18 -y &>>{LOG}
+statuscheck
+
+dnf install nodejs -y &>>{LOG}
+statuscheck
 
 id roboshop
 if [ $? -ne 0 ]
@@ -12,17 +16,18 @@ useradd roboshop
 fi
 
 mkdir -p /app
+statuscheck
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>{LOG}
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>{LOG}
 
 cd /app
-npm install
+npm install &>>{LOG}
 
-cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>{LOG}
 
-systemctl daemon-reload
+systemctl daemon-reload &>>{LOG}
 
 systemctl enable catalogue
 systemctl start catalogue
--- common.sh
+++ common.sh
@@ -1,1 +1,16 @@
 script_location=$(pwd)
+LOG=/tmp/roboshop.log
+status_check() {
+ if [ $? -eq 0 ]
+ then
+   echo -e "\e[1;32mSUCCESS\e[0m"
+ else
+   echo -e "\e[1;31mFAILURE\e[0m"
+   echo "Refer LOG file for more information , LOG -${LOG}"
+   exit 1
+ fi
+}
+
+print_head(){
+  echo -e "\e[35m$1\e[0m"
+}
