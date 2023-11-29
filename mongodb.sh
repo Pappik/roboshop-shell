7fd1c1113bcca749f6f8494c85d99b0fbfec39be
pappik <pappik@gmail.com>
2023-11-29 09:42:29 -0500

    route 53 json

---
+++ mongodb.sh
@@ -0,0 +1,13 @@
+script_location=$(pwd)
+
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+dnf install mongodb-org -y
+
+systemctl enable mongod
+
+systemctl start mongod
+
+sed 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+systemctl restart mongod
