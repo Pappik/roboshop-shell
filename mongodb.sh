32f8f4b264cb49eb250e83fd104365962b5cbcc1
pappik <pappik@gmail.com>
2023-11-29 12:08:19 -0500

    catalogue

--- mongodb.sh
+++ mongodb.sh
@@ -4,7 +4,7 @@ cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 dnf install mongodb-org -y
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
 
 systemctl enable mongod
 systemctl start mongod
