d9541f0194231adc32ec02bff9d53ddfdc9435f4
pappik <pappik@gmail.com>
2023-11-29 17:00:32 -0500

    catalogue

--- payment.sh
+++ payment.sh
@@ -2,3 +2,17 @@ source common.sh
 
 component=payment
 
+print_head "install python36"
+dnf install python36 gcc python3-devel -y &>>${LOG}
+status_check
+
+APP_REQ
+
+print_head "download dependencies"
+cd /app
+pip3.6 install -r requirements.txt &>>${LOG}
+status_check
+
+SYSTEMD
+
+
