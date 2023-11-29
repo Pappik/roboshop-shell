bea413add1d146d0482e5a56f76d8b01e6d8f6b5
pappik <pappik@gmail.com>
2023-11-29 12:57:39 -0500

    catalogue

---
+++ redis.sh
@@ -0,0 +1,20 @@
+source common.sh
+
+print_head "install redis repo"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${LOG}
+status_check
+
+print_head "enable redis"
+dnf module enable redis:remi-6.2 -y &>>${LOG}
+status_check
+
+print_head "install redis"
+dnf install redis -y &>>${LOG}
+status_check
+
+print_head "update redis listening ip"
+sed -i -e 's/127.0.0.1/0.0.0.0' /ect/redis.conf /etc/redis/redis.conf &>>${LOG}
+status_check
+
+systemctl enable redis
+systemctl start redis
